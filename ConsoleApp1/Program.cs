f8fb3c178023026a9aa16076b8b1d235de6de637
Will Mooreston <wmooreston@relias.com>
2018-12-01 14:37:10 -0500

    day 1, solution 2, solved!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,18 +10,50 @@ namespace DayOne
     {
         static void Main(string[] args)
         {   
-            string[] input = System.IO.File.ReadAllLines(args[0]);
 
+            // initial frequency of 0
             int freq = 0;
 
-            foreach (string line in input)
+            // store frequencies as they are calculated, including initial freq
+            List<int> freqs = new List<int>
             {
-                Console.WriteLine("input: " + line);
-                freq += int.Parse(line);
-                Console.WriteLine("\t freq now: " + freq);
-            }
+                freq
+            };
 
-            Console.WriteLine("final freq: " + freq);
+            //gather the inputs
+            string[] input = System.IO.File.ReadAllLines(args[0]);
+
+            //solution 2, keep looping on input until freq match is found
+            int loops = 0;
+            while (true)
+            {
+                foreach (string line in input)
+                {
+                    //Console.WriteLine("input: " + line);
+                    freq += int.Parse(line);
+                    //Console.WriteLine("\t freq now: " + freq);
+
+                    //solution 2 loop
+                    if (freqs.Contains(freq))
+                    {
+                        Console.WriteLine("Found recurrance of freq: " + freq);
+                        System.Environment.Exit(0);
+                       
+                    }
+                    else
+                    {
+                        freqs.Add(freq);
+                    }
+                }
+
+                Console.WriteLine("Loop " + loops + " complete. freqs[] now contains " + freqs.Count + " elements");
+                loops++;
+                //Console.WriteLine("press any key to continue");
+                //Console.ReadKey();
+            }
+            //solution 1 solve:
+            //Console.WriteLine("final freq: " + freq);
+
             Console.WriteLine("press any key to exit");
             Console.ReadKey();
 
