fed0d98515234cb9585c3197dd7f4f1c00e6abf5
Will Mooreston <wmooreston@relias.com>
2018-12-06 12:53:23 -0500

    day one, part two, solved in python!

--- PyDayOne/PyDayOne.py
+++ PyDayOne/PyDayOne.py
@@ -1,8 +1,23 @@
 
+file = "input.txt"
+
 freq = int(0)
-with open("input.txt") as input:
+with open(file) as input:
     for line in input:
         freq += int(line.split()[0])
 print("final freq: %d" % freq)
 
-
+freq = int(0)
+freqs = [freq]
+loops = int(0)
+while(True):
+    with open(file) as input:
+        for line in input:
+            freq += int(line.split()[0])
+            if (freq in freqs):
+                print("repeated freq: %d" % freq)
+                exit(0)
+            else:
+                freqs.append(freq)
+        print("completed loop # %d" % loops)
+        loops += 1
